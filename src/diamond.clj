5542e729a1d92887416c46ef4c93ea582da9cd7e
Eric Wilson <wilson.eric.n@gmail.com>
2012-10-19 06:03:00 -0400

    simplified grid method

--- src/diamond.clj
+++ src/diamond.clj
@@ -1,20 +1,18 @@
 (ns diamond)
 (use '[clojure.string :only (join)])
 
-(defn grid 
-  "Builds a n x n grid with values giving by evaluating f on the coordinates"
-  [n f]
-  (let [size (- (* 2 n) 1)]
-    (for [y (range size)]
-      (for [x (range size)] (f x y)))))
+;; Builds a n x n grid with values giving by evaluating f on the coordinates
+(defn grid [n f]
+  (for [y (range n)] (for [x (range n)] (f x y))))
 
-(defn taxicab-distance [[x1 y1] [x2 y2] ]
+(defn taxicab-distance [[x1 y1] [x2 y2]]
   (+ (Math/abs (- x1 x2)) (Math/abs (- y1 y2))))
 
 (defn diamond [n]
-  (grid n (fn [x y] (let [mid (- n 1) 
-                          num (- n (taxicab-distance [x y] [mid mid]))]
-                      (if (pos? num) num " ")))))
+  (grid (- (* 2 n) 1)
+           (fn [x y] (let [mid (- n 1) 
+                           num (- n (taxicab-distance [x y] [mid mid]))]
+                       (if (pos? num) num " ")))))
 
 (defn print-diamond [n]
   (println (join "\n" (map (partial join "") (diamond n)))))
